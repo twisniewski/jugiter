f90a6cf8b0b8c67d846c1ffca143437cf055087b
Tomasz Wisniewski <tomasz.wisniewski@allegrogroup.com>
2017-03-16 22:36:01 +0100

    #3 Cleaned up html generation

--- jugiter/static/tree.js
+++ jugiter/static/tree.js
@@ -1,83 +1,58 @@
+define([
+    'jquery',
+    'base/js/dialog',
+    'base/js/events',
+    'base/js/utils',
+    'base/js/namespace',
+    'tree/js/sessionlist'
+], function(
+    $,
+    dialog,
+    events,
+    utils,
+    IPython,
+    sessionlist) {
 
-define(['jquery', 'base/js/dialog', 'base/js/utils', 'base/js/namespace'], function ($, dialog, utils, IPython) {
+    var base_url = utils.get_body_data('baseUrl');
+    var clone_url = base_url + 'hello'
 
-  var base_url = utils.get_body_data('baseUrl');
-  var hello_url = base_url + 'hello'
-          function on_ok(){
-              // var re = /^\/notebooks(.*?)$/;
-              console.log("aa" + window.location.pathname);
+    var div = $('<div/>').html('<p>Please enter git repo address.</p><input type="text" name="gitrepo" id="gitrepo" size ="90">')
 
-              // display preloader during commit and push
-              // var preloader = '<img class="commit-feedback" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.8/ajax-loader.gif">';
-              // container.prepend(preloader);
-
-              // commit and push
-              // $.ajax(settings);
-          }
-
-      var p = $('<p/>').text("Please enter git repo address.")
-      var input = $('<textarea id="gitrepo" rows="4" cols="72"></textarea>')
-      var div = $('<div/>')
-      div.append(p).append(input)
-
-      var f=   function(){
-console.log("aa" + window.location.pathname);
+    var f = function() {
         dialog.modal({
-              body: div ,
-              title: 'Clone git repo',
-              buttons: {'Clone':
-                          { class:'btn-primary btn-large',
-                            click:function() {
-                $.post(base_url, {'add_dir':window.location.pathname, 'gitrepo': $('textara#gitrepo').text});
-            }
-                          },
-                        'Cancel':{}
-                  },
-          })
-        }
+            body: div,
+            title: 'Clone git repo',
+            buttons: {
+                'Clone': {
+                    class: 'btn-primary btn-large',
+                    click: function() {
+                        $.post(clone_url, {
+                            'add_dir': window.location.pathname,
+                            'gitrepo': $('#gitrepo').val()
+                        });
+                    }
+                },
+                'Cancel': {
+                    click: function() {
+                        $('#gitrepo').val('')
+                    }
+                }
+            },
+        })
+    }
 
-
-    var load_ipython_extension = function () {
-        console.log("tree2");
-
-
-        var insertAfter = function(node, newNode) {
-          node.parentNode.insertBefore(newNode, node.nextSibling)
-        }
-
-        var createLi = function(id) {
-          var newLi = $('<li>')
-            .attr('role', 'presentation')
-            .attr('id', id);
-          return newLi
-        }
-
-        var newDivider = createLi("git-divider")
-          .attr('class', 'divider');
-
-        var gitHeader = createLi('git-header')
-          .attr('class', 'dropdown-header').text("Git");
-
-        var gitClone = createLi('git-clone');
-        var gitCloneMenuItem = $('<a>')
-          .attr('role', 'menuitem')
-          .attr('tabindex', '-1')
-          .attr('href', '#')
-          .text("Clone")
-          .on('click', f);
-        gitClone.append(gitCloneMenuItem)
-
+    var load_ipython_extension = function() {
+        console.log("jugiter extension is on");
         var oldDivider = $('li.divider');
-        gitHeader.insertAfter(oldDivider)
-        gitClone.insertAfter(gitHeader)
-        newDivider.insertAfter(gitClone)
-
-
-
+        oldDivider.after('\
+          <li role="presentation" id="git-header" class="dropdown-header"> Git</li> \
+          <li role="presentation" id="git-clone"><a id="git-clone-a"role="menuitem" tabindex="-1" href="#" >Clone</a></li> \
+          <li role="presentation" class="divider"></li> \
+        ')
+        document.getElementById("git-clone-a").addEventListener('click', f)
     };
 
     return {
-
-        load_ipython_extension : load_ipython_extension,
+        load_ipython_extension: load_ipython_extension,
     };
 });
